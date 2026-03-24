6b2fcedf2d31ff1980d86ff88ce1f221c2c1212c
e2402241 <e2402241@vamk.fi>
2026-03-24 18:48:47 +0200

    1.9 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import BowlSeletion from "./components/BowlSelection"
+import BaseSelection from "./components/BaseSelection"
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className="min-h-screen flex flex-col bg-white font-sans">
       <Header />
       <BowlSeletion />
+      <BaseSelection />
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
       <Footer />
     </div>
