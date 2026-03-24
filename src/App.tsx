9cc9f75c177fa759cee917a2ebc4340b9d5798e6
e2402241 <e2402241@vamk.fi>
2026-03-24 19:10:18 +0200

    1.12 done, sorry näyttää viel fugly

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import BowlSeletion from "./components/BowlSelection"
 import BaseSelection from "./components/BaseSelection"
+import SummaryBar from "./components/SummaryBar"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <BowlSeletion />
       <BaseSelection />
+      <SummaryBar />
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
       <Footer />
     </div>
