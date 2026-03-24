72d0aad65fd3926801faa439f71fc0186f97a5b5
Otto Vandell <138617802+TastyBathwater@users.noreply.github.com>
2026-03-24 19:00:56 +0200

    9 task 19 select base right panel shel (#22)
    * 1.8 done
    
    * Forgot to put BowlSelection to App.tsk
    
    * 1.9 done
    
    * 1.9 done
    
    * 1.9 done
    
    ---------
    
    Co-authored-by: e2402241 <e2402241@vamk.fi>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import BowlSeletion from "./components/BowlSelection"
+import BaseSelection from "./components/BaseSelection"
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white font-sans">
       <Header />
+      <BowlSeletion />
+      <BaseSelection />
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
       <Footer />
     </div>
