a0394d6644ddae2ca579a709cbf9fc97cf095897
e2402241 <e2402241@vamk.fi>
2026-03-24 18:36:37 +0200

    Forgot to put BowlSelection to App.tsk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import BowlSeletion from "./components/BowlSelection"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white font-sans">
       <Header />
+      <BowlSeletion />
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
       <Footer />
     </div>
