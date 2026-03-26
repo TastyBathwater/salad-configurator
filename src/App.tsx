46f5388c576c83ef6c2dfed774430b0e89f0316c
blauculex <samaameer05@gmail.com>
2026-03-27 01:09:00 +0200

    2,2 yayy

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import CenterBowl from './components/CenterBowl';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import BowlSeletion from "./components/BowlSelection"
-import BaseSelection from "./components/BaseSelection"
-import SummaryBar from './components/SummaryBar';
-import IngredientSection from './components/IngredientSection';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import { Configurator } from './pages/Configurator';
+import { Community } from './pages/Community';
+import { Print } from './pages/Print';
 
 function App() {
-
-  return (
-    <div className="min-h-screen flex flex-col bg-white font-sans">
-      <Header />
-
-      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
-          <BowlSeletion />
-          <CenterBowl />
-          <BaseSelection />
-        </div>
-        <IngredientSection />
-        <SummaryBar />
-      </main>
-      <Footer />
-    </div>
-  )
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Configurator />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/print" element={<Print />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/Configurator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CenterBowl } from '../components/CenterBowl';
+
+export function Configurator() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <CenterBowl />
+        </div>
+    );
+}
