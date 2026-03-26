c45152ed0eef3c16e582983b5e827711a76f28fb
blauculex <samaameer05@gmail.com>
2026-03-26 22:36:06 +0200

    4 task 14 type definitions pricelistitem (#24)
    * muokkasin vähän koodia
    
    * feat: add CenterBowl component
    
    * yay
    
    ---------
    
    Co-authored-by: blauculex <samaameer5@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import BowlSeletion from "./components/BowlSelection"
-import BaseSelection from "./components/BaseSelection"
-import SummaryBar from "./components/SummaryBar"
-
+import React from 'react';
+import Header from "./components/Header";
+import { CenterBowl } from './components/CenterBowl';
 
 function App() {
-
-  return (
-    <div className="min-h-screen flex flex-col bg-white font-sans">
-      <Header />
-      <BowlSeletion />
-      <BaseSelection />
-      <SummaryBar />
-      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
-      <Footer />
-    </div>
-  )
+    return (
+        <>
+            <Header />
+            <main>
+                <CenterBowl />
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
