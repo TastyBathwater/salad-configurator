4896d4f56d73be00c7168657bc44b913f7749460
TastyBathwater <ottovandellz@gmail.com>
2026-03-27 09:41:45 +0200

    2.4 done vihdoinkin saan koodaa jipii

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -1,6 +1,15 @@
+import React, { useState } from 'react';
 import CenterBowl  from '../components/CenterBowl';
 
+interface Bowl {}
+interface Category {}
+interface Ingredient {}
+
 export function Configurator() {
+
+    const [bowls, setBowls] = useState<Bowl[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
     return (
         <div className="container mx-auto px-4 py-8">
             <CenterBowl />
