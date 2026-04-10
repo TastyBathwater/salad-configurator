4309e0efddbf873c5e0f2d156b0fd431a4b88ac1
TastyBathwater <ottovandellz@gmail.com>
2026-04-10 08:54:37 +0300

    3.4 done salee

--- src/components/IngredientSection.tsx
+++ src/components/IngredientSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Category, Ingredient } from "../types";
 import IngredientCard from "./IngredientCard";
 
@@ -8,6 +8,12 @@ type Props = {
 };
 
 const IngredientSection: React.FC<Props> = ({ categories, ingredients }) => {
+
+  const [activeCategory, setActiveCategory] = useState("all");
+  const handleCategoryClick = (category: string) => {
+    setActiveCategory(category);
+  };
+  
   return (
     <div className="bg-zinc-800 rounded-[3rem] p-8 text-white w-full shadow-lg">
       {/* Search Field */}
@@ -24,6 +30,7 @@ const IngredientSection: React.FC<Props> = ({ categories, ingredients }) => {
         {categories.map((category) => (
           <button
             key={category.id}
+            onClick={() => handleCategoryClick(category.name)}
             className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full"
           >
             {category.name}
